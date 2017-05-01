df395dba7f389b3732081506a501ff2f338e607a
Kevin Marais <kevin.n.marais@gmail.com>
2017-05-01 17:17:35 +0200

    Added debug script

---
+++ Assets/Scripts/DebugStuff.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugStuff : MonoBehaviour
+{
+	private void Update ()
+	{
+	    if (Input.GetKeyDown(KeyCode.R))
+	    {
+	        SceneManager.LoadScene(0);
+	    }
+
+	    if (Input.GetKeyDown(KeyCode.Escape))
+	    {
+	        Application.Quit();
+	    }
+	}
+}
